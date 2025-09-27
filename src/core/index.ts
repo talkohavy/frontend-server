d2548081c00ddac60e6055d190be0f1fb8eb1508
Tal Kohavy <talkohavy@gmail.com>
2025-09-27 23:15:02 +0300

    commit 4

---
+++ src/core/index.ts
@@ -0,0 +1,10 @@
+// singleton services initializer
+export { bootstrap } from './bootstrap';
+
+// Module registry initializer
+export { ModuleRegistry } from './moduleRegistry';
+
+// Core services already initialized
+export { callContextService } from './initCallContextService';
+export { logger } from './initLoggerService';
+export { configService } from './initConfigService';
